59c1f2a119d174fa523d8e69106d8a60f2356dab
Spencer Tipping <spencer@spencertipping.com>
2010-12-27 15:40:10 -0700

    Got the client written (no clue if it works though)

--- client/chalkspace.js
+++ client/chalkspace.js
@@ -1,18 +1,18 @@
-Chalkspace client | Spencer Tipping
-Licensed under the terms of the MIT source code license
+// Chalkspace client | Spencer Tipping
+// Licensed under the terms of the MIT source code license
 
-Introduction.
-Chalkspace is a collaborative 3D drawing environment. 3D is achieved by creating a plane (one exists already by default) and drawing on it. It isn't designed to be customizable. Rather, it's
-designed to provide a minimal set of options required to make editing reasonably easy. This includes things like changing the drawing plane's position, flipping it into the other axis, and
-moving the viewport within a 2D plane. That's about it though.
+// Introduction.
+// Chalkspace is a collaborative 3D drawing environment. 3D is achieved by creating a plane (one exists already by default) and drawing on it. It isn't designed to be customizable. Rather, it's
+// designed to provide a minimal set of options required to make editing reasonably easy. This includes things like changing the drawing plane's position, flipping it into the other axis, and
+// moving the viewport within a 2D plane. That's about it though.
 
-Model.
-The model just contains lines in 3D space. The user has an "active plane" that contains lines. It's designed so that every location on the screen maps to a UV-coordinate on the plane. The
-plane is a user-specific object; it isn't shared. The user has two options for transforming the plane. One is to translate it along its normal vector, and the other is to rotate it 90 degrees
-along the Y (vertical) axis.
+// Model.
+// The model just contains lines in 3D space. The user has an "active plane" that contains lines. It's designed so that every location on the screen maps to a UV-coordinate on the plane. The
+// plane is a user-specific object; it isn't shared. The user has two options for transforming the plane. One is to translate it along its normal vector, and the other is to rotate it 90 degrees
+// along the Y (vertical) axis.
 
-  Controls.
-  Shift-drag moves the viewport, the spacebar flips the plane, and the up/down arrows and the mousewheel move the plane along its normal vector. Dragging the mouse causes lines to be drawn.
+//   Controls.
+//   Shift-drag moves the viewport, the spacebar flips the plane, and the up/down arrows and the mousewheel move the plane along its normal vector. Dragging the mouse causes lines to be drawn.
 
   $(caterwaul.clone('std seq continuation montenegro vector')(function () {
     l*[b = $('body'), screen = html[canvas] /se[b.append(_)], rpc = caterwaul.montenegro.rpc /re[{listen: _('/listen'), draw: _('/draw')}]] in
@@ -26,13 +26,13 @@ along the Y (vertical) axis.
                _.draw_cursor() = _ /se[seq[cursor_offset *!o[_.line(cursor, cursor.plus(o)), _.line(cursor, cursor.plus(o, -1))]]],
                _.repaint()     = _ /se[_.clear(), seq[lines *!l[_.line(l.v1, l.v2)]], _.draw_cursor()],
 
-  Viewport geometry.
-  The canvas is considered to be at z = 0, which is 300 units away from the viewport. This gives a view angle of about 45 degrees on a small screen, and closer to 60 degrees on a large screen.
-  Reverse-projection is basically the ray-tracing algorithm; we need to find the UV coordinates of the intersection with the plane. The math is fairly simple. We can project the viewport into
-  the plane, then project the viewport-screen ray, and then multiply by the factor required to reach the plane. This gives us surface coordinates.
+//   Viewport geometry.
+//   The canvas is considered to be at z = 0, which is 300 units away from the viewport. This gives a view angle of about 45 degrees on a small screen, and closer to 60 degrees on a large screen.
+//   Reverse-projection is basically the ray-tracing algorithm; we need to find the UV coordinates of the intersection with the plane. The math is fairly simple. We can project the viewport into
+//   the plane, then project the viewport-screen ray, and then multiply by the factor required to reach the plane. This gives us surface coordinates.
 
-  Distance between the plane and a point is also simple. We just need to project the vector between a point on the plane and the point in question onto the plane's normal vector. That new
-  vector's length is the distance. (We can omit the normal vector measurement since we know it's a unit vector anyway.)
+//   Distance between the plane and a point is also simple. We just need to project the vector between a point on the plane and the point in question onto the plane's normal vector. That new
+//   vector's length is the distance. (We can omit the normal vector measurement since we know it's a unit vector anyway.)
 
                l*[plane_distance(v)  = v.plus(plane.normal.times(plane.d), -1).dot(plane.normal),
                   intersection(x, y) = l*[d = plane_distance(viewport), direction = view_at(x, y).plus(viewport, -1), dn = direction.dot(plane.normal)] in
@@ -58,3 +58,5 @@ along the Y (vertical) axis.
 
                       plane         = {normal: new caterwaul.vector(-0.8, 0.04, 0.4).unit(), d: 10} /se[_.flip() = _.normal = _.normal /re[new caterwaul.vector(-_.z, _.y, _.x)]],
                       viewport      = new caterwaul.vector(0, 0, -300)]]});
+
+// Generated by SDoc 
